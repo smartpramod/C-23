bb5335d6c10eadebd18485baff5149bef4e76a34
smartpramod <76101095+smartpramod@users.noreply.github.com>
2020-12-25 18:58:56 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,29 @@
+class Box{
+
+constructor(x,y,width,height)
+{
+    var body_options={
+
+        restitution : 0.8
+    }
+    this.body=Bodies.rectangle(x,y,width,height,body_options);
+    this.width=width;
+    this.height=height;
+    World.add(world,this.body);
+}
+display()
+{
+    var pos=this.body.position;
+    var angle=this.body.angle;
+    push ();
+    translate (pos.x,pos.y);
+    rotate (angle);
+    fill (255);
+    rectMode(CENTER);
+    rect(0,0,this.width,this.height);
+    pop ();
+}
+
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# C-23
-game
+# p5.play-boilerplate
+Boiler plate for p5.play
